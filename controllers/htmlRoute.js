0f3fe0bca87b8fddb36015adba588f893b5efbe2
Pillifino <dinodizon073100@gmail.com>
2022-05-24 08:49:28 -0700

    Homepage and login page loading handlebars properly! now onto adding singup and login feature!

--- controllers/htmlRoute.js
+++ controllers/htmlRoute.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const Blog  = require('../models/Blog');
 // const { Blog } = require('../../models');
 
+// route to get all blogs 
 router.get('/', async (req, res) => {
-    res.render('blog')
+  const blogData = await Blog.findAll().catch((err) => {
+    res.json(err);
+  });
+    const blogs = blogData.map((blog) => blog.get({plain: true}));
+    res.render('blog', { blogs });
   });
 
   router.get('/login', async (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+   // Otherwise, render the 'login' template
     res.render('login')
   });
   
